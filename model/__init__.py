6578b72769c4a575711adec00b82668359c9ca0f
askerlee <shaohua@gmail.com>
2022-05-22 23:26:16 +0800

    add laplacian loss

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,3 @@
-from model.denoising_diffusion_pytorch import GaussianDiffusion, Unet, Trainer
-
+from model.denoising_diffusion_pytorch import GaussianDiffusion, Unet
 from model.learned_gaussian_diffusion import LearnedGaussianDiffusion
 from model.weighted_objective_gaussian_diffusion import WeightedObjectiveGaussianDiffusion
