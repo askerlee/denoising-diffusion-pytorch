fix img normalization mean/std

model/utils.py
@@ -9,7 +9,6 @@ from PIL import Image
 import os
 import numpy as np
 import timm
-from timm.data.constants import IMAGENET_DEFAULT_STD, IMAGENET_DEFAULT_MEAN
 from .laplacian import LapLoss
 import imgaug.augmenters as iaa
 from torchvision.transforms import ColorJitter, ToTensor, ToPILImage, Resize
@@ -349,8 +348,9 @@ def sample_images(model, num_images, batch_size, dataset, img_save_path, nn_save
 # For ViTs, patch the original timm code to keep the spatial dimensions of the extracted image features.
 # use_head_feat: collapse geometric dimensions of the features.
 def timm_extract_features(model, x, use_head_feat=False):
-    img_mean = torch.tensor(IMAGENET_DEFAULT_STD, device=x.device).view(1, 3, 1, 1)
-    img_std  = torch.tensor(IMAGENET_DEFAULT_STD, device=x.device).view(1, 3, 1, 1)
+    # vit uses different mean/std from repvgg & resnet.
+    img_mean = torch.tensor(model.default_cfg['mean'], device=x.device).view(1, 3, 1, 1)
+    img_std  = torch.tensor(model.default_cfg['std'],  device=x.device).view(1, 3, 1, 1)
     # Imagenet-normalized x yields better features using an imagenet pretrained model.
     x = (x - img_mean) / img_std
 