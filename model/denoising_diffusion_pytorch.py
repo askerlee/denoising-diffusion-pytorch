fix image normalization bug

model/denoising_diffusion_pytorch.py
@@ -789,11 +789,13 @@ class GaussianDiffusion(nn.Module):
             img_gt1         = img_gt[:b2]
             img_gt2_dict    = self.dataset.sample_by_labels(classes1)
             img_gt2         = img_gt2_dict['img'].cuda()
+            img_gt2         = normalize_to_neg_one_to_one(img_gt2)
             # Replace the second half of img_gt with randomly sampled images 
             # that are of the same classes as img_gt1.
             img_gt          = torch.cat([img_gt1, img_gt2], dim=0)
             img_orig1       = img_orig[:b2]
             img_orig2       = img_gt2_dict['img_orig'].cuda()
+            img_orig2       = normalize_to_neg_one_to_one(img_orig2)
             img_orig        = torch.cat([img_orig1, img_orig2], dim=0)
             classes         = classes1.repeat(2)
 
@@ -1110,14 +1112,9 @@ class GaussianDiffusion(nn.Module):
             print0(f'height and width of image must be {img_size}')
             breakpoint()
         
-        if self.distillation_type == 'tfrac' and self.interp_loss_weight > 0:
-            assert b % 2 == 0
-            # Two sub-batches use the same random timesteps.
-            t = torch.randint(0, self.num_timesteps, (b//2, ), device=device).long().repeat(2)
-        else:
-            # t: random numbers of steps between 0 and num_timesteps - 1 (num_timesteps default is 1000)
-            # (b,): different random steps for different images in a batch.
-            t = torch.randint(0, self.num_timesteps, (b, ), device=device).long()
+        # t: random numbers of steps between 0 and num_timesteps - 1 (num_timesteps default is 1000)
+        # (b,): different random steps for different images in a batch.
+        t = torch.randint(0, self.num_timesteps, (b, ), device=device).long()
 
         img = normalize_to_neg_one_to_one(img)
         return self.p_losses(img, img_orig, t, classes, *args, **kwargs)

model/utils.py
@@ -132,7 +132,8 @@ class BaseDataset(data.Dataset):
 
         self.tv_transform = transforms.Compose([
             ToPILImage(),
-            ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.5/3.14),
+            Resize((image_size, image_size)),
+            ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.5/3.14),
             ToTensor()
         ])
 
@@ -145,7 +146,8 @@ class BaseDataset(data.Dataset):
         img_orig = self.test_transform(img)
 
         if self.training:
-            img_aug = self.geo_aug_func.augment_image(np.array(img)).copy()
+            img_aug = img
+            #img_aug = self.geo_aug_func.augment_image(np.array(img)).copy()
             img_aug = self.tv_transform(img_aug)
         else:
             img_aug = img_orig
@@ -355,6 +357,7 @@ def sample_images(model, num_images, batch_size, dataset, img_save_path, nn_save
 # For ViTs, patch the original timm code to keep the spatial dimensions of the extracted image features.
 # use_head_feat: collapse geometric dimensions of the features.
 def timm_extract_features(model, x, use_head_feat=False):
+    x = unnormalize_to_zero_to_one(x)
     # vit uses different mean/std from repvgg & resnet.
     img_mean = torch.tensor(model.default_cfg['mean'], device=x.device).view(1, 3, 1, 1)
     img_std  = torch.tensor(model.default_cfg['std'],  device=x.device).view(1, 3, 1, 1)