fix image normalization bug

model/utils.py
@@ -123,7 +123,7 @@ class BaseDataset(data.Dataset):
                     )),
                     iaa.Sometimes(perspect_prob, 
                                   iaa.PerspectiveTransform(scale=(0.01, 0.15), cval=(0,255), mode='constant')), 
-                    # iaa.Sometimes(0.3, iaa.GammaContrast((0.7, 1.7))),    # Gamma contrast degrades?
+                    iaa.Sometimes(0.3, iaa.GammaContrast((0.7, 1.7))),    # Gamma contrast degrades?
                     # When tgt_width==tgt_height, PadToFixedSize and CropToFixedSize are unnecessary.
                     # Otherwise, we have to take care if the longer edge is rotated to the shorter edge.
                     # iaa.PadToFixedSize(width=tgt_width,  height=tgt_height),
@@ -146,8 +146,7 @@ class BaseDataset(data.Dataset):
         img_orig = self.test_transform(img)
 
         if self.training:
-            img_aug = img
-            #img_aug = self.geo_aug_func.augment_image(np.array(img)).copy()
+            img_aug = self.geo_aug_func.augment_image(np.array(img)).copy()
             img_aug = self.tv_transform(img_aug)
         else:
             img_aug = img_orig